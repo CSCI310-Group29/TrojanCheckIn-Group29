3675444e98dcedae38cb944fe6197d23d57a2279
= <=>
2021-03-12 23:12:12 -0600

    updated repositories

--- app/src/main/java/com/csci310_group29/trojancheckincheckout/data/repo/UserRepoImpl.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/data/repo/UserRepoImpl.kt
@@ -2,17 +2,19 @@ package com.csci310_group29.trojancheckincheckout.data.repo
 
 import android.graphics.Bitmap
 import com.csci310_group29.trojancheckincheckout.data.models.User
+import io.reactivex.Completable
+import io.reactivex.Single
 
 class UserRepoImpl: UserRepository {
-    override fun updateProfile(firstName: String?, lastName: String?, major: String?, studentId: String?): User {
+    override fun updateProfile(firstName: String?, lastName: String?, major: String?, studentId: String?): Completable {
         TODO("Not yet implemented")
     }
 
-    override fun updateProfilePicture(profilePicture: Bitmap) {
+    override fun updateProfilePicture(profilePicture: Bitmap): Completable {
         TODO("Not yet implemented")
     }
 
-    override fun queryCheckedInUsers(buildingName: String?, user: User): List<User> {
+    override fun queryCheckedInUsers(buildingName: String?, user: User): Single<List<User>> {
         TODO("Not yet implemented")
     }
 }
