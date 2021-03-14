33bd83aac36f75e097cfe83cbb44354a965c1c70
Crystal Qin <clqin@usc.edu>
2021-03-13 18:16:27 -0800

    added ManagerHome and StudentProfile activities

---
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/views/StudentProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.csci310_group29.trojancheckincheckout.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.csci310_group29.trojancheckincheckout.R
+
+class StudentProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_profile)
+    }
+}
