55d6a3addce8da438d887179b50bac8adf9cc76d
= <=>
2021-03-20 04:54:56 -0500

    stuff

--- app/src/main/java/com/csci310_group29/trojancheckincheckout/data/datasource/remote/PictureFirebaseDataSource.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/data/datasource/remote/PictureFirebaseDataSource.kt
@@ -1,5 +1,6 @@
 package com.csci310_group29.trojancheckincheckout.data.datasource.remote
 
+import android.util.Log
 import com.csci310_group29.trojancheckincheckout.domain.repo.PicturesRepository
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -9,18 +10,27 @@ import javax.inject.Inject
 
 class PictureFirebaseDataSource @Inject constructor(): PicturesRepository {
 
+    companion object {
+        private val TAG = "PictureFirebaseDataSource"
+    }
+
     private val storage = FirebaseStorage.getInstance()
 
     override fun get(url: String): Single<ByteArray> {
+        Log.d(TAG, "Getting picture")
         return Single.create { emitter ->
             val storageRef = storage.reference
             val pictureRef = storageRef.child(url)
-            val ONE_MEGABYTE: Long = 1024 * 1024
-            pictureRef.getBytes(ONE_MEGABYTE)
-                .addOnSuccessListener { picture ->
+            pictureRef.stream
+                .addOnSuccessListener { downloadTask ->
+                    Log.d(TAG, "Successfully got image")
+                    val bytesTransferred = downloadTask.bytesTransferred
+                    val picture = ByteArray(bytesTransferred.toInt())
+                    downloadTask.stream.read(picture)
                     emitter.onSuccess(picture)
                 }
                 .addOnFailureListener { e ->
+                    Log.d(TAG, e.message.toString())
                     emitter.onError(e)
                 }
         }
@@ -28,13 +38,16 @@ class PictureFirebaseDataSource @Inject constructor(): PicturesRepository {
 
     override fun create(url: String, picture: ByteArray): Completable {
         return Completable.create { emitter ->
+            Log.d(TAG, "creating image")
             val storageRef = storage.reference
             val pictureRef = storageRef.child(url)
             pictureRef.putBytes(picture)
                 .addOnSuccessListener { taskSnapshot ->
+                    Log.d(TAG, "successfully created image")
                     emitter.onComplete()
                 }
                 .addOnFailureListener { e ->
+                    Log.d(TAG, e.message.toString())
                     emitter.onError(e)
                 }
         }
