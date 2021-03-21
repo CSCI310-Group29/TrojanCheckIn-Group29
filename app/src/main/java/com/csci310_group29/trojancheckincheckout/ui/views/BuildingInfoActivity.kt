a5adde0f9a170a7db631eb1ee9d3c3505f84c120
Crystal Qin <clqin@usc.edu>
2021-03-21 00:08:40 -0700

    building info

---
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/BuildingInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.csci310_group29.trojancheckincheckout.ui.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.csci310_group29.trojancheckincheckout.R
+
+class BuildingInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_building_info)
+    }
+}
