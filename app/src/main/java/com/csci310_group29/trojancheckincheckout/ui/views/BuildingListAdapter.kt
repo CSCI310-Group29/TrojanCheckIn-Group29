860b6e70d45738e93b3c704fc21629a61cc41571
Onyebuchi Obi <oobi@usc.edu>
2021-03-20 22:02:47 -0700

    Hello

---
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/BuildingListAdapter.kt
@@ -0,0 +1,52 @@
+package com.csci310_group29.trojancheckincheckout.ui.views
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.csci310_group29.trojancheckincheckout.R
+import com.csci310_group29.trojancheckincheckout.domain.models.Building
+
+class BuildingListAdapter(private val bList: List<Building>): RecyclerView.Adapter<BuildingListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(listItemView: View): RecyclerView.ViewHolder(listItemView){
+        val buildingNameText = itemView.findViewById<TextView>(R.id.building)
+        val numOfPeopleText = itemView.findViewById<TextView>(R.id.numOfPeople)
+        val numOfCapacityText = itemView.findViewById<TextView>(R.id.numOfCapacity)
+        val qrCodeText = itemView.findViewById<Button>(R.id.qr_code)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuildingListAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        val contactView = inflater.inflate(R.layout.activity_building_list, parent, false)
+
+        return ViewHolder(contactView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(buildingHolding: BuildingListAdapter.ViewHolder, position: Int) {
+
+        val building: Building = bList.get(position)
+
+        val bText = buildingHolding.buildingNameText
+        bText.setText(building.buildingName)
+
+        val numOfPeo = buildingHolding.numOfPeopleText
+        numOfPeo.setText(building.numPeople.toString())
+
+        val numOfCap = buildingHolding.numOfCapacityText
+        numOfCap.setText(building.capacity.toString())
+
+        val button = buildingHolding.qrCodeText
+    }
+
+    // Returns the total count of items in the list
+    override fun getItemCount(): Int {
+        return bList.size
+    }
+}
--- app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/ManagerHomeActivity.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/ManagerHomeActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.csci310_group29.trojancheckincheckout.R
 import com.csci310_group29.trojancheckincheckout.databinding.ActivityManagerHomeBinding
+import com.csci310_group29.trojancheckincheckout.domain.models.Building
 import com.csci310_group29.trojancheckincheckout.domain.models.User
 import com.csci310_group29.trojancheckincheckout.ui.viewmodels.ManagerHomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_student_home.*
 import java.lang.Exception
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 private const val TAG = "ManagerHomeActivity"
 
@@ -25,25 +27,26 @@ private const val TAG = "ManagerHomeActivity"
 @AndroidEntryPoint
 class ManagerHomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityManagerHomeBinding
+//    private lateinit var binding: ActivityManagerHomeBinding
 
     @Inject
     lateinit var viewModel: ManagerHomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var building: ArrayList<Building>
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_manager_home)
+        setContentView(R.layout.activity_manager_home)
 
-        binding = ActivityManagerHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityManagerHomeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
 //        binding.checkBuilding
 
-        val products = listOf("Building", "car", "truck")
-        products.forEach{
-            d("buchi", "Product is: $it")
-            binding.buildingList.append("$it \n\n")
-        }
+//        val products = listOf("Building", "car", "truck")
+//        products.forEach{
+//            d("buchi", "Product is: $it")
+//            binding.buildingList.append("$it \n\n")
+//        }
 
 
 
@@ -52,17 +55,19 @@ class ManagerHomeActivity : AppCompatActivity() {
     }
 
     fun onGetBuildings(view: View) {
+        startActivity(Intent(this, BuildingListAdapter::class.java))
 
-        try {
-           val rees = viewModel.getBuildingInformation()
-            Log.i(TAG, "$rees.")
-            binding.buildingList.apply {  }
 
-        } catch (e:Exception) {
-            Log.e(TAG, e.localizedMessage)
-            val toast = Toast.makeText(this, "Unable to get Building names", Toast.LENGTH_SHORT)
-            toast.show()
-        }
+//        try {
+//           val rees = viewModel.getBuildingInformation()
+//            Log.i(TAG, "$rees.")
+//            binding.buildingList.apply {  }
+//
+//        } catch (e:Exception) {
+//            Log.e(TAG, e.localizedMessage)
+//            val toast = Toast.makeText(this, "Unable to get Building names", Toast.LENGTH_SHORT)
+//            toast.show()
+//        }
 
 
     }
