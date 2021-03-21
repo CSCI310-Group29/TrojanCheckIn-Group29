a732d1b79c86ee466edff796a78cb45585eb250b
Crystal Qin <clqin@usc.edu>
2021-03-21 04:36:55 -0700

    lists in progress

--- app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/BuildingInfoActivity.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/BuildingInfoActivity.kt
@@ -1,49 +1,81 @@
-package com.csci310_group29.trojancheckincheckout.ui.views
-
-import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.csci310_group29.trojancheckincheckout.R
-import com.csci310_group29.trojancheckincheckout.data.datasource.remote.BuildingFirebaseDataSource
-import com.csci310_group29.trojancheckincheckout.domain.entities.BuildingEntity
-import io.reactivex.SingleObserver
-import io.reactivex.disposables.Disposable
-
-class BuildingInfoActivity : AppCompatActivity() {
-    val buildingRepo = BuildingFirebaseDataSource()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_building_info)
-
-
-        val rv = findViewById<View>(R.id.buildingInfo) as RecyclerView
+//package com.csci310_group29.trojancheckincheckout.ui.views
 //
-        val buildingInfo: List<BuildingEntity>
-        try {
-            val observable = buildingRepo.getAll()
-            observable.subscribe(object : SingleObserver<List<BuildingEntity>> {
-                override fun onSuccess(t: List<BuildingEntity>) {
-                    val buildings = t;
-                    val adapter = BuildingListAdapter(buildings)
-                    rv.adapter = adapter
-                }
-
-                override fun onSubscribe(d: Disposable) {
-
-                }
-
-                override fun onError(e: Throwable) {
-                    throw Exception("unable to get visit history")
-                }
-            })
-        } catch(e: Exception) {
-            val toast = Toast.makeText(this, "unable to get building info", Toast.LENGTH_SHORT)
-            toast.show()
-        }
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.Toast
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//import com.csci310_group29.trojancheckincheckout.R
+//import com.csci310_group29.trojancheckincheckout.domain.models.Building
+//import com.csci310_group29.trojancheckincheckout.domain.models.MutableBuilding
+//import com.csci310_group29.trojancheckincheckout.domain.usecases.BuildingUseCases
+//import dagger.hilt.android.AndroidEntryPoint
+//import io.reactivex.SingleObserver
+//import io.reactivex.disposables.Disposable
+//import javax.inject.Inject
 //
-        rv.layoutManager = LinearLayoutManager(this)
-    }
-}
+//@AndroidEntryPoint
+//class BuildingInfoActivity : AppCompatActivity() {
+//
+//
+//    @Inject
+//    lateinit var buildingDomain: BuildingUseCases
+//
+//    lateinit var rv: RecyclerView
+//    lateinit var map: HashMap<String, Int>
+//    lateinit var adapter: BuildingListAdapter
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_building_info)
+//
+//
+//        rv = findViewById<View>(R.id.buildingInfo) as RecyclerView
+////
+//        var buildingInfo: MutableList<MutableBuilding>
+//
+//        try {
+//            val observable = buildingDomain.getAllBuildings()
+//            observable.subscribe(object : SingleObserver<List<Building>> {
+//                override fun onSuccess(t: List<Building>) {
+//                    //buildingInfo = initializeList(t)
+//                    //adapter = BuildingListAdapter(buildingInfo)
+//                    //rv.adapter = adapter
+//                }
+//
+//                override fun onSubscribe(d: Disposable) {
+//
+//                }
+//
+//                override fun onError(e: Throwable) {
+//                    throw Exception("unable to get visit history")
+//                }
+//            })
+//
+//        } catch(e: Exception) {
+//            val toast = Toast.makeText(this, "unable to get building info", Toast.LENGTH_SHORT)
+//            toast.show()
+//        }
+////
+//        rv.layoutManager = LinearLayoutManager(this)
+//    }
+//
+//    //doesnt work lol
+//    /*fun initializeHashmap(list: List<Building>) {
+//        for(b in list)
+//    }*/
+//    /*
+//
+//
+//    fun initializeList(list: List<Building>): MutableList<MutableBuilding> {
+//        val res: MutableList<MutableBuilding> = mutableListOf<MutableBuilding>()
+//        for(b in list) {
+//            res.add( MutableBuilding(b.id,b.buildingName,b.address,b.capacity, b.numPeople,
+//                b.qrCodeRef))
+//        }
+//
+//        return res
+//    }*/
+//
+//}
--- app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/StudentHistoryActivity.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/StudentHistoryActivity.kt
@@ -2,50 +2,66 @@
 package com.csci310_group29.trojancheckincheckout.ui.views
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.csci310_group29.trojancheckincheckout.R
-import com.csci310_group29.trojancheckincheckout.data.datasource.fake.VisitFakeDataSource
 import com.csci310_group29.trojancheckincheckout.domain.models.Visit
+import com.csci310_group29.trojancheckincheckout.domain.query.VisitQuery
+import com.csci310_group29.trojancheckincheckout.domain.usecases.VisitUseCases
 import com.csci310_group29.trojancheckincheckout.ui.viewmodels.Session
+import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class StudentHistoryActivity : AppCompatActivity() {
-    val visitRepo = VisitFakeDataSource()
+
+    val TAG = "StudentHistoryActivity"
+
+    @Inject
+    lateinit var visitDomain: VisitUseCases
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_student_history)
-//
-//        val rv = findViewById<View>(R.id.visitHistory) as RecyclerView
-//
-//        val visits: List<Visit>
-//        try {
-//            val observable = visitRepo.queryVisits(Session.user!!, Visit(null, null, null, null))
-//            observable.subscribe(object : SingleObserver<List<Visit>> {
-//                override fun onSuccess(t: List<Visit>) {
-//                    val visits = t;
-//                    val adapter = VisitHistoryAdapter(visits)
-//                    rv.adapter = adapter
-//                }
-//
-//                override fun onSubscribe(d: Disposable) {
-//
-//                }
-//
-//                override fun onError(e: Throwable) {
-//                    throw Exception("unable to get visit history")
-//                }
-//            })
-//        } catch(e: Exception) {
-//            val toast = Toast.makeText(this, "unable to get visit history", Toast.LENGTH_SHORT)
-//            toast.show()
-//        }
-//
-//        rv.layoutManager = LinearLayoutManager(this)
+        setContentView(R.layout.activity_student_history)
+
+        val rv = findViewById<View>(R.id.visitHistory) as RecyclerView
+
+        val visits: List<Visit>
+        try {
+            //val uQuery = UserQuery(Session.user!!.firstName,Session.user!!.lastName, Session.user!!.major,
+                //Session.user!!.studentId, (Session.user!!.checkedInBuilding == null), Session.user!!.isStudent)
+
+            val vQuery = VisitQuery(null,null,null,null,null,null)
+            val observable = visitDomain.getUserVisitHistory(Session.uid,vQuery)
+            observable.subscribe(object : SingleObserver<List<Visit>> {
+                override fun onSuccess(t: List<Visit>) {
+                    Log.i(TAG, "${Session.uid}")
+                    Log.i(TAG,"${t.size}")
+                    val visits = t;
+                    val adapter = VisitHistoryAdapter(visits)
+                    rv.adapter = adapter
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+                    throw Exception("unable to get visit history")
+                }
+            })
+        } catch(e: Exception) {
+            val toast = Toast.makeText(this, "unable to get visit history", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        rv.layoutManager = LinearLayoutManager(this)
 
 
     }
