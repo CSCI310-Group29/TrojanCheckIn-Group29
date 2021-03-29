830b9a9e5137a8ee5a19ba5a2a5de4e2edf06787
Arian Rahbar <arahbar@usc.edu>
2021-03-29 04:35:20 -0500

    added register view model test cases

--- app/src/test/java/com/csci310_group29/trojancheckincheckout/ui/viewmodels/RegisterViewModelTest.kt
+++ app/src/test/java/com/csci310_group29/trojancheckincheckout/ui/viewmodels/RegisterViewModelTest.kt
@@ -1,37 +1,125 @@
 package com.csci310_group29.trojancheckincheckout.ui.viewmodels
-//import android.util.Log
-//import androidx.lifecycle.ViewModel
-//import com.csci310_group29.trojancheckincheckout.domain.entities.UserEntity
-//import com.csci310_group29.trojancheckincheckout.domain.usecases.AuthUseCases
-//import io.reactivex.Completable
-//import io.reactivex.CompletableObserver
-//import io.reactivex.disposables.Disposable
-//import javax.inject.Inject
-//
-//@RunWith(MockitoJUnitRunner::class)
-//class RegisterViewModel @Inject constructor(private val authDomain: AuthUseCases): ViewModel() {
-//
-//
-//    @Mock
-//    private lateinit var mockRegister: register
-//
-//
-//    @Before
-//
-//    @Test
-//    public fun register(email:String, password:String, user: UserEntity): Completable {
-//
-//    `when`(mockRegister.register()).thenReturn(Completable.complete())
-//
-//    }
-//
-//
-//    //@Test
-//  //  private fun getEmailDomain(email: String): String {
-//
-//
-//
-//  //  }
-//
-//
-//}
+
+import com.csci310_group29.trojancheckincheckout.domain.entities.UserEntity
+import com.csci310_group29.trojancheckincheckout.domain.usecases.AuthUseCases
+import io.reactivex.Completable
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class RegisterViewModelTest {
+
+    @Mock
+    private lateinit var mockAuthUseCases: AuthUseCases
+
+    private lateinit var registerViewModel: RegisterViewModel
+
+    @Before
+    fun setup() {
+        registerViewModel = RegisterViewModel(mockAuthUseCases)
+
+    }
+
+    @Test
+    fun registerStudentSuccessfullyTest() {
+        val email = "test@usc.edu"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "1234567890", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity)
+    }
+
+    @Test
+    fun registerStudentWithNonUSCEmailTest() {
+        val email = "test@gmail.com"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "1234567890", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    @Test
+    fun registerStudentWithNoLastNameTest() {
+        val email = "test@usc.edu"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "", "Computer Science",
+            null, "1234567890", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    @Test
+    fun registerStudentWithNoStudentIdTest() {
+        val email = "test@usc.edu"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    @Test
+    fun registerStudentWithNoPasswordTest() {
+        val email = "test@usc.edu"
+        val password = ""
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "1234567890", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    @Test
+    fun registerStudentWithInvalidStudentIdLengthTest() {
+        val email = "test@usc.edu"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "123456789", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    @Test
+    fun registerStudentWithNonDigitStudentIdTest() {
+        val email = "test@usc.edu"
+        val password = "password"
+        val userEntity = UserEntity(
+            null, true, "Tommy", "Trojan", "Computer Science",
+            null, "123456789A", null
+        )
+        `when`(mockAuthUseCases.signup(email, password, userEntity)).thenReturn(Completable.complete())
+        register(email, password, userEntity, true)
+    }
+
+    private fun register(email: String, password: String, userEntity: UserEntity, expectError: Boolean = false) {
+        val completable = registerViewModel.register(email, password, userEntity)
+        try {
+            completable.blockingAwait()
+            if (expectError) {
+                fail("expected an exception when trying to register")
+            }
+        } catch(e: Exception) {
+            if (!expectError) {
+                fail("got an exception when trying to register: ${e.localizedMessage}")
+            }
+        }
+    }
+}
