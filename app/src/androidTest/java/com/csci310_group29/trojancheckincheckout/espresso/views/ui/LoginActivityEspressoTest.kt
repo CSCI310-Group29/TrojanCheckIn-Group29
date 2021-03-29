c27385799a08c5ce2098be9398f84b03c3c49009
Eugene Lee <elee9889@usc.edu>
2021-03-28 21:07:05 -0400

    LoginActivityEspressoTest done, toast test working, removed ToastMatcher

--- app/src/androidTest/java/com/csci310_group29/trojancheckincheckout/espresso/views/ui/LoginActivityEspressoTest.kt
+++ app/src/androidTest/java/com/csci310_group29/trojancheckincheckout/espresso/views/ui/LoginActivityEspressoTest.kt
@@ -3,8 +3,8 @@ package com.csci310_group29.trojancheckincheckout.espresso.views.ui
 import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Root
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
@@ -53,15 +53,12 @@ class LoginActivityEspressoTest {
         }
     }
 
-    fun isToast(): Matcher<Root?>? {
-        return ToastMatcher()
-    }
 
     @Test
     // Tests that valid student login succeeds
     fun validLoginStudent() {
-        onView(withId(R.id.emailInput)).perform(typeText(validEmailStudent))
-        onView(withId(R.id.passwordInput)).perform(typeText(validPasswordStudent))
+        onView(withId(R.id.emailInput)).perform(typeText(validEmailStudent), closeSoftKeyboard())
+        onView(withId(R.id.passwordInput)).perform(typeText(validPasswordStudent), closeSoftKeyboard())
         onView(withId(R.id.loginButton)).perform(click())
         Thread.sleep(5000)
 
@@ -72,8 +69,8 @@ class LoginActivityEspressoTest {
     @Test
     // Tests that valid manager login succeeds
     fun validLoginManager() {
-        onView(withId(R.id.emailInput)).perform(typeText(validEmailManager))
-        onView(withId(R.id.passwordInput)).perform(typeText(validPasswordManager))
+        onView(withId(R.id.emailInput)).perform(typeText(validEmailManager), closeSoftKeyboard())
+        onView(withId(R.id.passwordInput)).perform(typeText(validPasswordManager), closeSoftKeyboard())
         onView(withId(R.id.loginButton)).perform(click())
         Thread.sleep(5000)
 
@@ -84,21 +81,17 @@ class LoginActivityEspressoTest {
     @Test
     // Tests incorrect credentials will show error Toast
     fun invalidLoginToast() {
-        onView(withId(R.id.emailInput)).perform(typeText(invalidEmail))
-        onView(withId(R.id.passwordInput)).perform(typeText(invalidPassword))
+        onView(withId(R.id.emailInput)).perform(typeText(invalidEmail), closeSoftKeyboard())
+        onView(withId(R.id.passwordInput)).perform(typeText(invalidPassword), closeSoftKeyboard())
         onView(withId(R.id.loginButton)).perform(click())
 
         // Should show toast "Unable to login: email or password incorrect"
         val toastString = "email or password incorrect"
-//        onView(withText("email or password incorrect"))
-//            .inRoot(ToastMatcher()).check(matches(isDisplayed()))
         onView(withText(toastString))
             .inRoot(RootMatchers.withDecorView(not(decorView)))
             .check(matches(isDisplayed()))
-//        onView(withText(toastString))
-//            .inRoot(withDecorView(not(activityRule.onActivity.getWindow().getDecorView())))
-//            .check(matches(isDisplayed()))
     }
+
 
     @After
     fun tearDown() {
