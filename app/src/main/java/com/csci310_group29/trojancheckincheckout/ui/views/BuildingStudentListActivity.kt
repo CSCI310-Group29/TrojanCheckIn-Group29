337ac07955816d68dfd11e05f8a821ed626c520f
Eugene Lee <elee9889@usc.edu>
2021-04-11 22:12:26 -0400

    progress on students in building

---
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/BuildingStudentListActivity.kt
@@ -0,0 +1,107 @@
+package com.csci310_group29.trojancheckincheckout.ui.views
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.csci310_group29.trojancheckincheckout.R
+import com.csci310_group29.trojancheckincheckout.domain.models.Building
+import com.csci310_group29.trojancheckincheckout.domain.models.MutableBuilding
+import com.csci310_group29.trojancheckincheckout.domain.models.User
+import com.csci310_group29.trojancheckincheckout.domain.usecases.BuildingUseCases
+import com.csci310_group29.trojancheckincheckout.domain.usecases.UserUseCases
+import com.csci310_group29.trojancheckincheckout.ui.viewmodels.BuildingStudentListAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.Observer
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.Disposable
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class BuildingStudentListActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userDomain: UserUseCases
+
+    lateinit var rv: RecyclerView
+    var map: HashMap<String, Int> = HashMap<String, Int>()
+    lateinit var adapter: BuildingStudentListAdapter
+    lateinit var studentList: ArrayList<User>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_building_student_list)
+
+        rv = findViewById<View>(R.id.buildingStudentList) as RecyclerView
+
+        try {
+            val observable = userDomain.observeUsersInBuilding(intent.getStringExtra("buildingName").toString())
+            observable.subscribe(object : SingleObserver<List<User>> {
+                override fun onSuccess(t: List<User>) {
+                    studentList = initializeList(t)
+                    initializeHashmap(t)
+                    adapter = BuildingStudentListAdapter(userDomain, studentList)
+                    rv.adapter = adapter
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                    throw Exception("Unable to get Student List")
+                }
+            })
+        } catch(e: Exception) {
+            val toast = Toast.makeText(this, "Unable to get Student List", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        rv.LayoutManager = LinearLayoutManager(this)
+    }
+
+    fun initializeHashmap(list: List<User>) {
+        var i = 0
+        for(b in list) {
+            map.put(b.id, i)
+            i++
+        }
+    }
+
+    fun initializeList(list: List<User>): ArrayList<User> {
+        val res: ArrayList<User> = ArrayList<User>()
+        for(u in list) {
+            res.add( User(u.id,
+                            u.isStudent,
+                            u.email,
+                            u.firstName,
+                            u.lastName,
+                            u.major,
+                            u.checkedInBuilding,
+                            u.studentId,
+                            u.deleted,
+                            u.profilePicture))
+
+
+            val observable = userDomain.observeUserById(u.id)
+            observable.subscribe(object: Observer<List<User>> {
+                override fun onComplete() { }
+                override fun onSubscribe(d: Disposable) { }
+                override fun onNext(t: User) {
+                    //
+                }
+
+                override fun onError(e: Throwable) { }
+            })
+
+        }
+
+        return res
+    }
+
+}
---
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/views/ManagerStudentProfileActivity.kt
@@ -0,0 +1,79 @@
+package com.csci310_group29.trojancheckincheckout.ui.views
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.csci310_group29.trojancheckincheckout.R
+import com.csci310_group29.trojancheckincheckout.domain.models.User
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_manager_student_profile.*
+import javax.inject.Inject
+
+private const val TAG = "ManagerStudentProfileActivity"
+
+@AndroidEntryPoint
+class ManagerStudentProfileActivity : AppCompatActivity() {
+
+//    @Inject
+//    lateinit var viewModel: ManagerStudentProfileViewModel
+
+    lateinit var pb: ProgressBar;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manager_student_profile)
+
+        pb = findViewById(R.id.MSPindeterminateBar)
+        loadingStart()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        val userObserver = Observer<User> { newUser ->
+            Log.i(TAG, "in Manager Student Profile")
+            MSPfirstName.text = newUser.firstName
+            MSPlastName.text = newUser.lastName
+            MSPmajor.text = newUser.major
+            MSPstudentId.text = newUser.studentId
+            if(newUser.deleted != null && newUser.deleted!!) {
+                MSPcurrBuilding.text = "Deleted"
+            } else {
+                if (newUser.checkedInBuilding != null) {
+                    MSPcurrBuilding.text = newUser.checkedInBuilding!!.buildingName
+                } else {
+                    MSPcurrBuilding.text = ""
+                }
+            }
+            MSPsProfilePic.setImageBitmap(toBitmap(newUser.profilePicture))
+            loadingEnd()
+
+        }
+    }
+
+    fun onViewHistory(view: View) {
+        startActivity(Intent(this, StudentProfileActivity::class.java))
+    }
+
+
+    fun loadingStart() {
+        pb!!.visibility = ProgressBar.VISIBLE
+    }
+
+    fun loadingEnd() {
+        pb!!.visibility = ProgressBar.INVISIBLE
+    }
+
+    private fun toBitmap(bArray: ByteArray?): Bitmap? {
+        if(bArray == null) {
+            return null;
+        }
+        return BitmapFactory.decodeByteArray(bArray,0, bArray.size)
+    }
+
+}
