130596b7cb50cb932931bdc70ac55ecac5c1dd56
Eugene Lee <elee9889@usc.edu>
2021-04-30 00:08:40 -0400

    CSV parsed and connected to backend

--- app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/viewmodels/ManagerUpdateCapacityViewModel.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/ui/viewmodels/ManagerUpdateCapacityViewModel.kt
@@ -2,24 +2,16 @@ package com.csci310_group29.trojancheckincheckout.ui.viewmodels
 
 import android.net.Uri
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
-import com.csci310_group29.trojancheckincheckout.domain.models.Building
-import com.csci310_group29.trojancheckincheckout.domain.models.User
+import com.csci310_group29.trojancheckincheckout.domain.models.BuildingOperation
+import com.csci310_group29.trojancheckincheckout.domain.models.Operation
 import com.csci310_group29.trojancheckincheckout.domain.usecases.BuildingUseCases
-import com.fasterxml.jackson.dataformat.csv.CsvSchema
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.opencsv.bean.CsvBindByName
-import com.opencsv.bean.CsvToBean
-import com.opencsv.bean.CsvToBeanBuilder
-import com.vhl.blackmo.grass.dsl.grass
+import com.opencsv.CSVReader
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.CompletableObserver
 import io.reactivex.rxjava3.disposables.Disposable
-import java.io.BufferedReader
-import java.io.File
 import java.io.FileReader
 import java.nio.file.Paths
 import javax.inject.Inject
@@ -36,7 +28,7 @@ class ManagerUpdateCapacityViewModel @Inject constructor(private val buildingDom
     fun updateWithUI(buildingId: String, newCapacity: Double): Completable {
         return Completable.create { emitter ->
             val observe = buildingDomain.updateSingleBuildingCapacity(buildingId, newCapacity)
-            observe.subscribe(object: CompletableObserver {
+            observe.subscribe(object : CompletableObserver {
                 override fun onComplete() {
                     emitter.onComplete()
                 }
@@ -58,7 +50,7 @@ class ManagerUpdateCapacityViewModel @Inject constructor(private val buildingDom
      */
 
     /**
-     * Pass List<String> of `operations` to UseCase
+     * Pass List<BuildingOperation> to UseCase
      */
     fun updateWithCSV(uri: Uri): Completable {
         Log.i(TAG, "Curr directory: " + Paths.get("").toAbsolutePath().toString())
@@ -66,31 +58,64 @@ class ManagerUpdateCapacityViewModel @Inject constructor(private val buildingDom
         Log.i(TAG, "Is relative URI? " + uri.isRelative)
         return Completable.create { emitter ->
 //            var buildingMap: HashMap<String, Double> = HashMap()
-            var opList: MutableList<String> = ArrayList()
+            var opList: MutableList<BuildingOperation> = ArrayList()
 
             try {
 //                Log.i(TAG, "Inside ViewModel. CSV URI: " + uri.getPath())
-                val parsedUri = uri.getPath()!!.replace("/document/raw:","")
+                val parsedUri = uri.getPath()!!.replace("/document/raw:", "")
                 Log.i(TAG, "Inside ViewModel. Parsed URI: " + parsedUri)
                 Log.i(TAG, "Successfully opened file")
-                csvReader().open(parsedUri) {
-                    readAllAsSequence().forEach { row ->
-                        val x: String = row[0]
-                        if(x != "U" && x != "A" && x != "R") {
-                            throw Exception("Invalid operation")
-                        }
-                        val y: String = row[1]
-                        var s: String = x + "," + y
-                        if(x != "R") {
-                            val z: Double = row[1].toDouble()
-                            s += "," + z
-                        }
+//                csvReader().open(parsedUri) {
+//                    readAllAsSequence().forEach { row ->
+//                        val x: String = row[0]
+//                        if(x != "U" && x != "A" && x != "R") {
+//                            throw Exception("Invalid operation")
+//                        }
+//                        var o: Operation
+//                        if(x == "U") o = Operation.UPDATE
+//                        else if(x == "A") o = Operation.ADD
+//                        else o = Operation.REMOVE
+//
+//                        val y: String = row[1]
+//
+//                        var op: BuildingOperation
+//                        if(x != "R") {
+//                            val z: Int = row[2].toInt()
+//                            op = BuildingOperation(o, y, z)
+//                        } else {
+//                            op = BuildingOperation(o, y)
+//                        }
+//
+////                        buildingMap.put(b, c)
+//                        opList.add(op)
+//                    }
+//                }
 
-//                        buildingMap.put(b, c)
-                        opList.add(s)
+                val reader = CSVReader(FileReader(parsedUri))
+                val rows = reader.readAll()
+                for (r in rows) {
+                    val x: String = r[0]
+                    if(x != "U" && x != "A" && x != "R") {
+                        throw Exception("Invalid operation")
                     }
-                }
+                    var o: Operation
+                    if(x == "U") o = Operation.UPDATE
+                    else if(x == "A") o = Operation.ADD
+                    else o = Operation.REMOVE
 
+                    val y: String = r[1]
+
+                    var op: BuildingOperation
+                    if(x != "R") {
+                        val z: Int = r[2].toInt()
+                        if(z < 1) throw Exception("Invalid operation")
+                        op = BuildingOperation(o, y, z)
+                    } else {
+                        op = BuildingOperation(o, y)
+                    }
+
+                    opList.add(op)
+                }
 
                 // Debugging
 //                Log.i(TAG, "Building List size: " + buildingMap.size)
@@ -106,24 +131,24 @@ class ManagerUpdateCapacityViewModel @Inject constructor(private val buildingDom
 
                 Log.i(TAG, "Successfully processed file")
 
-            } catch(e: Exception) {
-                Log.e(TAG,"Cannot read file")
+            } catch (e: Exception) {
+                Log.e(TAG, "Cannot read file")
                 Log.e(TAG, "   Error: " + e.printStackTrace())
                 emitter.onError(e)
             } finally {
                 try {
 //                    fileReader!!.close()
                     Log.i(TAG, "Successfully read and closed file")
-                } catch(e: Exception) {
+                } catch (e: Exception) {
                     Log.e(TAG, "Cannot close file")
                     emitter.onError(e)
                 }
             }
 
 //            val observe = buildingDomain.updateMultipleBuildingCapacities(buildingMap)
-            val observe = buildingDomain.FUNCTION(opList)
+            val observe = buildingDomain.processBuildingOperations(opList)
 
-            observe.subscribe(object: CompletableObserver {
+            observe.subscribe(object : CompletableObserver {
                 override fun onComplete() {
                     emitter.onComplete()
                 }
