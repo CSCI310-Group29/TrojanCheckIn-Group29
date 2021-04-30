70241aebe9a91cf9edcdbff0063be3060e17dc54
Arian Rahbar <arahbar@usc.edu>
2021-04-29 20:12:43 -0500

    added use case called processBuildingOperations

--- app/src/main/java/com/csci310_group29/trojancheckincheckout/domain/models/BuildingOperation.kt
+++ app/src/main/java/com/csci310_group29/trojancheckincheckout/domain/models/BuildingOperation.kt
@@ -1,3 +1,16 @@
 package com.csci310_group29.trojancheckincheckout.domain.models
 
-//data class BuildingOperation()
+enum class Operation { ADD, UPDATE, REMOVE }
+
+/*
+Class for adding stuff from CSV
+
+Operation is a enum with UPDATE, REMOVE, and ADD.
+For UPDATE and ADD, capacity must not be null. For
+REMOVE, capacity can be null
+ */
+data class BuildingOperation(
+    val operation: Operation,
+    val buildingName: String,
+    val capacity: Int? = null
+)
